9487e63297ccdf476469ddc2c731d8798c30e66c
Hossam Tamri <hossam.tamri@nist.gov>
2023-04-10 11:20:04 -0400

    Update versions and compatibility versions

--- darq-webapp/qdar-analysis-client/src/app/modules/aggregate-detections-file/components/adf-merge-dialog/adf-merge-dialog.component.ts
+++ darq-webapp/qdar-analysis-client/src/app/modules/aggregate-detections-file/components/adf-merge-dialog/adf-merge-dialog.component.ts
@@ -18,7 +18,7 @@ export class AdfMergeDialogComponent implements OnInit {
   fileSelection: IADFDescriptor[] = [];
   compatibilityVersions = [
     ['2.0.0', '2.0.0-SNAPSHOT', '2.0.1', '2.0.2', '2.0.3'],
-    ['2.1.0', '2.1.1'],
+    ['2.1.0', '2.1.1', '2.1.2'],
   ];
 
   name: string;
